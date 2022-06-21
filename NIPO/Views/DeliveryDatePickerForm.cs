762a19eb07899722d2a7200327694ac0d1d5e9a1
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-06-21 11:21:50 +0900

    納期を選択する空フォームを追加。#3

---
+++ NIPO/Views/DeliveryDatePickerForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NIPO.Views
+{
+    public partial class DeliveryDatePickerForm : Form
+    {
+        public DeliveryDatePickerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

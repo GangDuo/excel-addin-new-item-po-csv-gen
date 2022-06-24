63ae1978c523a10634e2e2a3181500fc5fc60241
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-06-24 13:22:30 +0900

    ボタンのラベル名を変更。

--- NIPO/Views/DeliveryDatePickerForm.Designer.cs
+++ NIPO/Views/DeliveryDatePickerForm.Designer.cs
@@ -55,7 +55,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "OK";
+            this.button1.Text = "確定";
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2

a7a10487cf53f2c44e99058c28f67d98a7257ced
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-07-01 18:10:39 +0900

    プロパティに注釈を付ける。#10

--- NIPO/Models/DeliveryDatePicker.cs
+++ NIPO/Models/DeliveryDatePicker.cs
@@ -1,11 +1,18 @@
 ﻿
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NIPO.Models
 {
     public class DeliveryDatePicker
     {
+        [Display(Name = "納期", Description = "重複を排除した納期。", Order = 1)]
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd")]
+        [UIHint("Calendar")]
         public DateTime? Value { get; private set; }
+
+        [Display(Name = "有効", Description = "チェック済みをCSV生成対象にする。", Order = 2)]
+        [UIHint("CheckBox")]
         public bool IsEnabled { get; set; }
 
         public DeliveryDatePicker(DateTime? value, bool isEnabled)
